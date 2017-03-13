526701c4e70c80fb64bf177b2ece2011c469d346
hoangnl <nguyenlehoang@hotmail.com>
2017-03-13 17:23:41 +0700

    cập nhật

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
